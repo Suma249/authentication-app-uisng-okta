ae0aea136929f8b3611b32a06c19c1192b25aba8
Suma249 <3584_suma@katalystindia.org>
2024-10-13 08:32:52 +0530

    added code for navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' exact={true} component={Home} />
+            <Route path='/' exact={true} component={Staff} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
